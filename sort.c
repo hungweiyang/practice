ed0a24ae0edad5169a3b23103fe1b9548e32cf65
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2020-04-07 09:03:35 +0800

    Add quicksort

--- sort.c
+++ sort.c
@@ -1,21 +1,14 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
-int main()
+void swap(int *a, int *b)
 {
-    int a[] = {7, 5, 4, 3, 1, 0, 2, 8, 10, -1, 100};
-    int len = sizeof(a)/sizeof(a[0]);
-    
-    for (int i = 0; i < len; i++) printf("%d ", a[i]);
-    printf("\n");
-    
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void bubblesort(int a[], int len)
+{
     for (int i = 0; i < len - 1; i++)
     {
         for (int j = 0; j < len - i - 1; j++)
@@ -28,6 +21,43 @@ int main()
             }    
         }
     }
+}
+
+void quicksort(int a[], const int front, const int end)
+{
+    if (front >= end)
+        return;
+    
+    int *pivot = &a[end];
+    int i = front-1;
+    int j = front;
+    
+    for (; j != end; j++)
+    {
+        if (a[j] < *pivot)
+        {
+            i++;
+            swap(&a[i], &a[j]);
+        }
+    }
+    
+    i++;
+    swap(&a[i], pivot);
+    
+    quicksort(a, front, i-1);
+    quicksort(a, i+1, end);
+}
+
+int main()
+{
+    int a[] = {77, 55, -44, 33, 11, 0, 2, 8, 10, -1, 30};
+    int len = sizeof(a)/sizeof(a[0]);
+    
+    for (int i = 0; i < len; i++) printf("%d ", a[i]);
+    printf("\n");
+    
+    //bubblesort(a, len);
+    quicksort(a, 0, len-1);
     
     for (int i = 0; i < len; i++) printf("%d ", a[i]);
     printf("\n");
