7eeaf052e61995f40e75c96ed286243519e274f8
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2022-03-31 08:30:34 +0000

    Add insertion sort

--- sort.c
+++ sort.c
@@ -61,6 +61,22 @@ void bubblesort(int a[], int len)
     }
 }
 
+void bubblesort2(int a[], int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i; j < len - 1; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                int t = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }    
+        }
+    }
+}
+
 void quicksort(int a[], const int front, const int end)
 {
     if (front >= end)
@@ -86,6 +102,22 @@ void quicksort(int a[], const int front, const int end)
     quicksort(a, i+1, end);
 }
 
+void insertion_sort(int a[], int len)
+{
+    for (int i = 1; i < len; ++i) {
+        int key = a[i];
+        int j;
+        for (j = i-1; j >= 0; --j) {
+            if (a[j] > key) {
+                a[j+1] = a[j];
+            } else {
+                break;
+            }
+        }
+        a[j+1] = key;
+    }    
+}
+
 int main()
 {
     int a[] = {77, 55, -44, 33, 11, 0, 2, 8, 10, -1, 30};
@@ -94,9 +126,10 @@ int main()
     for (int i = 0; i < len; i++) printf("%d ", a[i]);
     printf("\n");
 
-    mergesort(a, len);
-    //bubblesort(a, len);
+    //mergesort(a, len);
+    //bubblesort2(a, len);
     //quicksort(a, 0, len-1);
+    insertion_sort(a, len);    
     
     for (int i = 0; i < len; i++) printf("%d ", a[i]);
     printf("\n");
