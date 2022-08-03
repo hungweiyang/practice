373fe062e705ba44c54466cd4e5fbff489f0021e
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2022-08-03 07:49:37 +0000

    Add bit_hacks

--- interview_question.c
+++ interview_question.c
@@ -1,57 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverse_bits()
-{
-  // method 1
-  // for (int i = 0; i < 8; i++)
-  // {
-  //   r |= (v & (1<<i)) >> i << (7-i);
-  // }
-
-  // method 2
-  // const int s = sizeof(v) * 8 - 1;
-  // for (int i = 0; i < s; i++)
-  // {
-  //   r |= v & 0x1;
-  //   r <<= 1;
-  //   v >>= 1;
-  // }
-
-  // method 3
-  // unsigned char v = 0b01010101;
-  // unsigned char r = 0;
-  // int s = sizeof(v) * 8 - 1;
-  // for (;v;)
-  // {
-  //   r |= v & 0x1;
-  //   r <<= 1;
-  //   v >>= 1;
-  //   --s;
-  // }
-  // r <<= s;
-
-  // method 4
-  // http://graphics.stanford.edu/~seander/bithacks.html#BitReverseObvious
-  // eliminated one iteration of the for loop
-  unsigned char v = 0b10101;
-  unsigned char r = v;  // get LSB while assigning
-  int s = sizeof(v) * 8 - 1;
-  for (v >>= 1; v; v >>= 1)
-  {
-    r <<= 1;
-    r |= v & 0x1;
-    --s;
-  }
-  r <<= s;
-  
-  for (int i = sizeof(r)*8-1; i >= 0; i--)
-  {
-    printf("%d", (r >> i) & 1);    
-  }
-  printf("\n");
-}
-
 // void question_multiple_three()
 // {
 //     int target, i = 2;
