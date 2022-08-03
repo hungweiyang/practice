766e77689dca116a8a523998a14dbf4694242273
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2022-08-03 07:45:14 +0000

    Add better version of reverse_bit()

--- interview_question.c
+++ interview_question.c
@@ -3,14 +3,13 @@
 
 void reverse_bits()
 {
-  unsigned char v = 0b01010101;
-  unsigned char r = 0;
-  
-  for (int i = 0; i < 8; i++)
-  {
-    r |= (v & (1<<i)) >> i << (7-i);
-  }
+  // method 1
+  // for (int i = 0; i < 8; i++)
+  // {
+  //   r |= (v & (1<<i)) >> i << (7-i);
+  // }
 
+  // method 2
   // const int s = sizeof(v) * 8 - 1;
   // for (int i = 0; i < s; i++)
   // {
@@ -19,7 +18,38 @@ void reverse_bits()
   //   v >>= 1;
   // }
 
-  printf("%x\n", r);
+  // method 3
+  // unsigned char v = 0b01010101;
+  // unsigned char r = 0;
+  // int s = sizeof(v) * 8 - 1;
+  // for (;v;)
+  // {
+  //   r |= v & 0x1;
+  //   r <<= 1;
+  //   v >>= 1;
+  //   --s;
+  // }
+  // r <<= s;
+
+  // method 4
+  // http://graphics.stanford.edu/~seander/bithacks.html#BitReverseObvious
+  // eliminated one iteration of the for loop
+  unsigned char v = 0b10101;
+  unsigned char r = v;  // get LSB while assigning
+  int s = sizeof(v) * 8 - 1;
+  for (v >>= 1; v; v >>= 1)
+  {
+    r <<= 1;
+    r |= v & 0x1;
+    --s;
+  }
+  r <<= s;
+  
+  for (int i = sizeof(r)*8-1; i >= 0; i--)
+  {
+    printf("%d", (r >> i) & 1);    
+  }
+  printf("\n");
 }
 
 // void question_multiple_three()
