622db437645b1e79fc680ff65a9a9640fd10c944
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2023-04-07 12:03:03 +0000

    Add mergesort

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 
 def bubblesort():
   size = len(nums)
@@ -7,7 +8,41 @@ def bubblesort():
       if nums[j] > nums[j+1]:
         nums[j], nums[j+1] = nums[j+1], nums[j]
 
-nums = [4, 5, 1, 2, 3]
+def helper(begin: int, end: int):
+  if begin >= end:
+    return
+  mid = begin + (end - begin) / 2
+  mid = int(mid)
+  helper(begin, mid)
+  helper(mid+1, end)
+
+  temp = []
+  i, j = begin, mid+1
+  while i <= mid and j <= end:
+    if nums[i] < nums[j]:
+      temp.append(nums[i])
+      i = i+1
+    else:
+      temp.append(nums[j])
+      j = j+1
+
+  if i == mid+1:
+    while j <= end:
+      temp.append(nums[j])
+      j = j+1
+  else:
+    while i <= mid:
+      temp.append(nums[i])
+      i = i+1
+
+  for i in range(begin, end+1):
+    nums[i] = temp[i-begin]
+      
+def mergesort():
+  helper(0, len(nums)-1)
+
+#nums = [4, 5, 1, 2, 3]
+nums = [random.randint(-100, 100) for i in range(10)]
 print(nums)
-bubblesort()
+mergesort()
 print(nums)
